0e8e7040b900347a020ae370adfbb9131fc7611e
Michael2001papis <dvnka2@gmail.com>
2026-08-12 21:33:34 +0300

    01.01.01

--- frontend/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-declare module "plotly.js-dist-min";
-declare module "virtual:pwa-register";
-
-interface ImportMetaEnv {
-  readonly VITE_API_URL?: string;
-}
